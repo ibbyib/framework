2d10c97a08453e48c091493c308e68d399e26bbb
Andrew Baker <baker.ja.andrew@gmail.com>
2014-05-19 21:41:38 -0400

    Elements Added to Scaffold
    Added navbars and grids.

--- scaffold/header.php
+++ scaffold/header.php
@@ -7,14 +7,13 @@
   <title>Framework Demo</title>
   <script src="js/jquery-1.11.0.min.js"></script>
   <script src="js/javvy.js"></script>
-  <link rel="stylesheet" href="css/frames.css" />
-  <link rel="stylesheet" href="css/demo.css" />
+  <link rel="stylesheet" href="syslib/sysstyle/sysstyle.min.css" />
   <!--[if lt IE 9]>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
   <![endif]--> <!-- -->
 </head>
 <body>
-  <div class="fixedheader">
+  <div class="navBar">
     Work in progress!!
   </div>
